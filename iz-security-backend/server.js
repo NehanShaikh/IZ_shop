704d2214ff20264f4d942544e449f2966f0ad6f0
NehanShaikh <156279957+NehanShaikh@users.noreply.github.com>
2026-02-13 21:33:15 +0530

    first commit

--- iz-security-backend/server.js
+++ iz-security-backend/server.js
@@ -17,12 +17,8 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MySQL
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
+const db = mysql.createConnection(process.env.DATABASE_URL);
+
 
 db.connect((err) => {
   if (err) {
